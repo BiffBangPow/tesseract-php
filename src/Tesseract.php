f0cba7f4b1293d114c10760575c5a4e531126e36
William Arslett <william.a@biffbangpow.com>
2017-05-04 16:17:25 +0100

    Implemented createCall method

--- src/Tesseract.php
+++ src/Tesseract.php
@@ -2,6 +2,7 @@
 
 namespace BiffBangPow\TesseractPHP;
 
+use BiffBangPow\TesseractPHP\Exception\TesseractAPIException;
 use BiffBangPow\TesseractPHP\Exception\UserAuthenticationException;
 
 class Tesseract
@@ -26,6 +27,12 @@ class Tesseract
         $this->soapClient = $soapClient;
     }
 
+    /**
+     * @param string $userID
+     * @param string $password
+     * @param string $dataSource
+     * @throws UserAuthenticationException
+     */
     public function authenticateUser(string $userID, string $password, string $dataSource)
     {
         $success = false;
@@ -37,10 +44,33 @@ class Tesseract
             'bSuccess' => &$success
         ]);
 
-        if ($success) {
-            $this->securityToken = $result;
-        } else {
+        if (!$success) {
             throw new UserAuthenticationException($result);
         }
+
+        $this->securityToken = $result;
+    }
+
+    /**
+     * @param string $callXML
+     * @return int - New Call Number
+     */
+    public function createCall(string $callXML): int
+    {
+        $success = false;
+        $callNum = 0;
+
+        $result = $this->soapClient->__soapCall("Create_Call", [
+            'sDataIn' => $callXML,
+            'sTokenID' => $this->securityToken,
+            'iNewCallNum' => &$callNum,
+            'bSuccess' => &$success
+        ]);
+
+        if (!$success) {
+            throw new TesseractAPIException($result);
+        }
+
+        return $callNum;
     }
 }
--- test/TesseractTest.php
+++ test/TesseractTest.php
@@ -2,6 +2,7 @@
 
 namespace Test\BiffBangPow\TesseractPHP;
 
+use BiffBangPow\TesseractPHP\Exception\TesseractAPIException;
 use BiffBangPow\TesseractPHP\Exception\UserAuthenticationException;
 use BiffBangPow\TesseractPHP\Tesseract;
 use Mockery as m;
@@ -13,7 +14,7 @@ class TesseractTest extends \PHPUnit_Framework_TestCase
 
     public function testAuthenticateUser_CallsSoapClient()
     {
-        $soapClient = $this->mockSoapClient(true);
+        $soapClient = $this->mockSoapClient();
         $tesseract = new Tesseract($soapClient);
         $userId = "john";
         $password = "password123";
@@ -22,16 +23,18 @@ class TesseractTest extends \PHPUnit_Framework_TestCase
         $tesseract->authenticateUser($userId, $password, $dataSource);
 
         $soapClient->shouldHaveReceived("__soapCall")
-            ->with("AuthenticateUser", $this->parameters([
-                'sUID' => 'string',
-                'sPWD' => 'string',
-                'sDataSource' => 'string',
-                'bSuccess' => 'boolean'
+            ->with("AuthenticateUser", $this->parametersWithRequired([
+                'sUID'        => $userId,
+                'sPWD'        => $password,
+                'sDataSource' => $dataSource
+            ], [
+                'bSuccess'
             ]))
-            ->once();
+            ->once()
+        ;
     }
 
-    public function testAuthenticateUser_InvalidParameters_ThrowsException()
+    public function testAuthenticateUser_UnSuccessfulRequest_ThrowsException()
     {
         $soapClient = $this->mockSoapClient(false);
         $tesseract = new Tesseract($soapClient);
@@ -43,24 +46,83 @@ class TesseractTest extends \PHPUnit_Framework_TestCase
         $tesseract->authenticateUser($userId, $password, $dataSource);
     }
 
+    public function testCreateCall_CallsSoapClient()
+    {
+        $callXML = "<Call>xml...</Call>";
+
+        $soapClient = $this->mockSoapClient();
+        $soapClient->shouldReceive("__soapCall")
+            ->with("Create_Call", $this->parametersWithReferencesToBeReturned([
+                'iNewCallNum' => 5,
+                'bSuccess' => true
+            ]))
+        ;
+
+        $tesseract = new Tesseract($soapClient);
+
+        $tesseract->authenticateUser("john", "password123", "data_source");
+        $tesseract->createCall($callXML);
+
+        $soapClient->shouldHaveReceived("__soapCall")
+            ->with("Create_Call", $this->parametersWithRequired([
+                'sDataIn'  => $callXML,
+                'sTokenID' => self::SECURITY_TOKEN
+            ], [
+                'iNewCallNum',
+                'bSuccess'
+            ]))
+            ->once();
+    }
+
+    public function testCreateCall_SuccessfulRequest_ReturnsNewCallNum()
+    {
+        $callNum = 5;
+
+        $soapClient = $this->mockSoapClient();
+        $soapClient->shouldReceive("__soapCall")
+            ->with("Create_Call", $this->parametersWithReferencesToBeReturned([
+                'iNewCallNum' => $callNum,
+                'bSuccess' => true
+            ]))
+        ;
+
+        $tesseract = new Tesseract($soapClient);
+
+        $tesseract->authenticateUser("john", "password123", "data_source");
+        $result = $tesseract->createCall("<Call>xml...</Call>");
+
+        $this->assertSame($callNum, $result);
+    }
+
+    public function testCreateCall_UnSuccessfulRequest_ThrowsException()
+    {
+        $soapClient = $this->mockSoapClient();
+        $soapClient->shouldReceive("__soapCall")
+            ->with("Create_Call", $this->parametersWithReferencesToBeReturned([
+                'bSuccess' => false
+            ]))
+        ;
+
+        $tesseract = new Tesseract($soapClient);
+
+        $tesseract->authenticateUser("john", "password123", "data_source");
+
+        $this->setExpectedException(TesseractAPIException::class);
+        $tesseract->createCall("<Call>xml...</Call>");
+
+    }
+
     /**
-     * @param string $securityToken
+     * @param bool $authenticateUser
      * @return m\MockInterface|\SoapClient
      */
     private function mockSoapClient(bool $authenticateUser = true)
     {
-
         $soapClient = m::mock(\SoapClient::class);
         $soapClient->shouldReceive('__soapCall')
             ->with(
                 "AuthenticateUser",
-                m::on(function($parameters) use ($authenticateUser) {
-                    if (!is_array($parameters)) {
-                        return false;
-                    }
-                    $parameters['bSuccess'] = $authenticateUser;
-                    return true;
-                })
+                $this->parametersWithReferencesToBeReturned(['bSuccess' => $authenticateUser])
             )
             ->andReturn(self::SECURITY_TOKEN)
         ;
@@ -69,18 +131,45 @@ class TesseractTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @param array $requiredValues
+     * @param array $requiredReferences
      * @return m\Matcher\Closure
      */
-    private function parameters($required)
+    private function parametersWithRequired($requiredValues, $requiredReferences = [])
     {
-        return m::on(function ($parameters) use ($required) {
+        return m::on(function ($parameters) use ($requiredValues, $requiredReferences) {
+            if (!is_array($parameters)) {
+                throw new \Exception('$parameters is not an array');
+                return false;
+            }
+            foreach ($requiredValues as $name => $value) {
+                if(!array_key_exists($name, $parameters) || $parameters[$name] !== $value) {
+                    throw new \Exception("Parameter '$name' with the value '$value' was not included");
+                    return false;
+                }
+            }
+            foreach ($requiredReferences as $name) {
+                if(!array_key_exists($name, $parameters)) {
+                    throw new \Exception("Parameter reference '$name' was not included");
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
+
+    /**
+     * @param array $referencesToSet
+     * @return m\Matcher\Closure
+     */
+    private function parametersWithReferencesToBeReturned(array $referencesToSet)
+    {
+        return m::on(function ($parameters) use ($referencesToSet) {
             if (!is_array($parameters)) {
                 return false;
             }
-            foreach ($required as $name => $type) {
-                if(!array_key_exists($name, $parameters) || gettype($parameters[$name]) !== $type) {
-                    return false;
-                }
+            foreach ($referencesToSet as $reference => $value) {
+                $parameters[$reference] = $value;
             }
             return true;
         });
